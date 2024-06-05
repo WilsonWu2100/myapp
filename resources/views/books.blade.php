ae0440ff475869c59239d8132d2c716b0094adcb
Wilson Wu <wwu@doghouse.agency>
2024-06-05 17:41:20 +1000

    Add all book images

--- resources/views/books.blade.php
+++ resources/views/books.blade.php
@@ -30,7 +30,9 @@
             @foreach($books as $book)
                 <tr>
                     <td>{{ $book['id'] }}</td>
-                    <td>{{ $book['image'] }}</td>
+                    <td>
+                        <img src="{{ $book['image'] }}" alt="book image" width="88px" height="120px"/>
+                    </td>
                     <td>{{ $book['name'] }}</td>
                     <td>{{ $book['isbn'] }}</td>
                     <td>{{ $book['author'] }}</td>
